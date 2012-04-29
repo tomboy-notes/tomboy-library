65d72a3ea375413f74f26cac15477d8bb18c9e93
Robert Nordan <rpvn@robpvn.net>
2012-04-29 19:15:18 +0200

    Fixed some license info & solution standard header

--- Tomboy-library/Tomboy-library-tests/ReaderTests.cs
+++ Tomboy-library/Tomboy-library-tests/ReaderTests.cs
@@ -2,13 +2,13 @@
 //  ReaderTests.cs
 //  
 //  Author:
-//       rpvn <${AuthorEmail}>
+//       Robert Nordan <rpvn@robpvn.net>
 //  
-//  Copyright (c) 2012 rpvn
+//  Copyright (c) 2012 Robert Nordan
 // 
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
+//  the Free Software Foundation, either version 2.1 of the License, or
 //  (at your option) any later version.
 // 
 //  This program is distributed in the hope that it will be useful,
