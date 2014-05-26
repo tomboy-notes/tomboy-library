b0b5609fa0d52a760144ce4782126b00a783c300
Timo Dörr <timo@latecrew.de>
2014-05-26 11:25:10 +0200

    Fix failing unit tests
    * Additionally don't use DiskStorage to test Xml Notes, but use
      XmlNoteReader

--- Tomboy-library/Tomboy-library-tests/NoteXmlTests.cs
+++ Tomboy-library/Tomboy-library-tests/NoteXmlTests.cs
@@ -20,7 +20,6 @@
 // 
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-using System;
 using NUnit.Framework;
 using System.IO;
 using Tomboy.Xml;
@@ -30,21 +29,24 @@ namespace Tomboy
 	[TestFixture()]
 	public class NoteXmlTests
 	{
-		//TODO: Make this independent of Diskstorage
+		private Note ReadNoteFromFile (string path, string uri)
+		{
+			return XmlNoteReader.Read (File.ReadAllText (path), uri);
+		}
+
 		[Test()]
-		[ExpectedException (typeof (TomboyException),
-		 ExpectedMessage = "Note XML is corrupted!")] //TODO: This message subject to change!
+		[ExpectedException (typeof (System.Xml.XmlException))]
 		public void Read_NonsenseNoteFile_ThrowsException ()
 		{
 			string StartHereNotePath = "../../test_notes/corrupt_notes/nonsense.note";
-			DiskStorage.Read (StartHereNotePath, "tomboy://nonsense");
+			this.ReadNoteFromFile (StartHereNotePath, "tomboy://nonsense");
 		}
 
 		[Test()]
 		public void Read_ProperNoteFile_CorrectTitle ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 
 			Assert.AreEqual ("Start Here", toCheck.Title);
 		}
@@ -52,7 +54,7 @@ namespace Tomboy
 		public void Read_ProperNoteFile_CorrectText ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 
 			Assert.That (toCheck.Text.Contains ("Welcome to Tomboy!"));
 		}
@@ -60,7 +62,7 @@ namespace Tomboy
 		public void Read_ProperNoteFile_HyperTagsArePreserved ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 
 			Assert.That (toCheck.Text.Contains ("<bold>"));
 		}
@@ -68,28 +70,28 @@ namespace Tomboy
 		public void Read_ProperNoteFile_CreateDateMatches ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 			Assert.AreEqual (toCheck.CreateDate.Ticks, 633770466544871750);
 		}
 		[Test()]
 		public void Read_ProperNoteFile_LastChangeDateMatches ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 			Assert.AreEqual (toCheck.ChangeDate.Ticks, 634683883142191587);
 		}
 		[Test()]
 		public void Read_ProperNoteFile_LastMetadataChangeDateMatches ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 			Assert.AreEqual (toCheck.MetadataChangeDate.Ticks, 634683883142221590);
 		}
 		[Test()]
 		public void Read_ProperNoteFile_WidthAndHeightMatches ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 			Assert.AreEqual (toCheck.Width, 450);
 			Assert.AreEqual (toCheck.Height, 360);
 		}
@@ -97,7 +99,7 @@ namespace Tomboy
 		public void Read_ProperNoteFile_XAndYMatches ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 			Assert.AreEqual (toCheck.X, 1305);
 			Assert.AreEqual (toCheck.Y, 93);
 		}
@@ -105,16 +107,16 @@ namespace Tomboy
 		public void Read_ProperNoteFile_OpenOnStartupMatches ()
 		{
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note toCheck = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
-			Assert.AreEqual (toCheck.OpenOnStartup, false);
-			Assert.IsInstanceOfType (typeof(bool), toCheck.OpenOnStartup);
+			Note toCheck = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Assert.AreEqual ("False", toCheck.OpenOnStartup);
+			Assert.IsInstanceOf<string> (toCheck.OpenOnStartup);
 		}
 
 		[Test]
 		public void ReadWrite_Note ()
 		{ 
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note sampleNote = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note sampleNote = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 			
 			string note_xml = XmlNoteWriter.Write (sampleNote);
 			Note toCheck = XmlNoteReader.Read (note_xml, sampleNote.Uri);
@@ -132,10 +134,10 @@ namespace Tomboy
 			// note body. The size and link prefixed tags are CUSTOM namespaces. For valid XML, these have to be
 			// defined, and this is what we test.
 			string StartHereNotePath = "../../test_notes/proper_notes/90d8eb70-989d-4b26-97bc-ba4b9442e51f.note";
-			Note sampleNote = DiskStorage.Read (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
+			Note sampleNote = ReadNoteFromFile (StartHereNotePath, "tomboy://90d8eb70-989d-4b26-97bc-ba4b9442e51f");
 			
 			sampleNote.Text = "<size:huge>About</size:huge><link:url>http://www.google.com</link:url>";
-			string note_xml = XmlNoteWriter.Write (sampleNote);
+			XmlNoteWriter.Write (sampleNote);
 		}
 	}
 }
